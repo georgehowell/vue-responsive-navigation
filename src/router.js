f7733b58da4bb5464ba074375718dd52a0fd0490
George Howell <geordiehowell@gmail.com>
2020-10-16 15:42:10 +1100

    first commit for blog post with butter-CMS

--- src/router.js
+++ src/router.js
@@ -77,13 +77,19 @@ export default new Router({
             component: () => { return import ('./views/CSSandJavascript.vue') }
         },
         {
-            path: '/blog',
-            name: 'blog',
+            path: '/blog/',
+            name: 'blog-home',
             // route level code-splitting
             // this generates a separate chunk (about.[hash].js) for this route
             // which is lazy-loaded when the route is visited.
-            component: () => { return import ('./views/Blog.vue') }
+            component: () => { return import ('./views/BlogHome.vue') }
         },
+        {
+            path: '/blog/:slug',
+            name: 'example-post',
+            component: () => { return import ('./views/ExamplePost') }
+        },
+      
         {
             path: '/portfolio',
             name: 'portfolio',
