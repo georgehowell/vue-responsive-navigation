10acae63eb908d62a68a4f5767b88d45d3770b72
George Howell <geordiehowell@gmail.com>
2020-10-17 07:47:33 +1100

    <router-link :to= instead of <a href= on Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="home">
-    <a href="/gsap"><h2>GSAP</h2></a>
-    <a href="/css"><h2>CSS3</h2></a>
-    <a href="/more-css"><h2>More CSS3</h2></a>
-    <a href="/css-again"><h2>CSS3</h2></a>
-    <a href="/css-glitch"><h2>CSS3 Glitch</h2></a>
-    <a href="/css-and-javascript"><h2>CSS3 + Javascript</h2></a>
+    <router-link :to="'/gsap'"><h2>GSAP</h2></router-link>
+    <router-link :to="'/css'"><h2>CSS3</h2> </router-link>
+    <router-link :to="'/more-css'"><h2>More CSS3</h2> </router-link>
+    <router-link :to="'/css-again'"><h2>CSS3</h2> </router-link>
+    <router-link :to="'/css-glitch'"><h2>CSS3 Glitch</h2> </router-link>
+    <router-link :to="'/css-and-javascript'"><h2>CSS3 + Javascript</h2> </router-link>
   </div>
 </template>
 
